e3c3162b958aadefa52fcd1bdbb7c384ea452d77
kislow <kadirislow@gmail.com>
2021-05-03 13:49:45 +0200

    refactor vscode_install script

--- vscode/src/install_vscode_izzo.sh
+++ vscode/src/install_vscode_izzo.sh
@@ -1,16 +1,19 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# Update packages and install dependencies
-sudo apt update -y && apt install software-properties-common apt-transport-https wget
-
-#import the Microsoft GPG key
-wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
-
-# enable vscode repo
-sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
-
-# Install latest vscode version
-sudo apt update -y && apt install code
+vscode=$(code --version | awk '{print $1; exit}')
+code_version=$?
+if [ $code_version -eq 0 ]; then
+    echo "VSCode version $vscode installed"
+else
+  # Update packages and install dependencies
+  sudo apt update -y && apt install software-properties-common apt-transport-https wget
+  #import the Microsoft GPG key
+  wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -
+  # enable vscode repo
+  sudo add-apt-repository "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main"
+  # Install latest vscode version
+  sudo apt update -y && apt install code
+fi
 
 #install extensions
 cat ./src/extensions.txt | while read extension || [[ -n $extension ]];
